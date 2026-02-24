5afc6eaa2a76fd5e8d02a78105d942e22f3efc2f
Bhupendra Singh <bs4004908@gmail.com>
2026-02-24 18:33:48 +0530

    add files

---
+++ server/models/Blog.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const blogSchema = new mongoose.Schema({
+title: String,
+description: String,
+category: String,
+image: String,
+isPublished: {
+    type: Boolean,
+    default: false,
+},
+}, { timestamps: true })
+
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+export default Blog;
